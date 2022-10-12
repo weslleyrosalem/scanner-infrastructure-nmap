a4c9fcb6a57243037762074625eadc6b984f2e0e
Weslley Rosalem <wesleysntn@gmail.com>
2022-10-11 23:33:01 -0300

    simples

---
+++ bruno.js
@@ -0,0 +1,13 @@
+[
+    {
+        "name": "nmap",
+        "context": "BodgeIt",
+        "target": {
+            "name": "BodgeIt",
+            "location": "10.128.2.55",
+            "attributes": {
+                "NMAP_PARAMTER": "-Pn"
+            }
+        }
+    }
+]
